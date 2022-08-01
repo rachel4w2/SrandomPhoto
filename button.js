2346e5fa13dabc70eb416f44b127cecc596963ac
rachel4w2 <87238236+rachel4w2@users.noreply.github.com>
2022-08-01 15:11:49 +0900

    Change6

---
+++ button.js
@@ -0,0 +1,17 @@
+const button1 = document.querySelector("button:nth-child(1)");
+const button2 = document.querySelector("button:nth-child(2)");
+const button3 = document.querySelector("button:nth-child(3)");
+const button4 = document.querySelector("button:nth-child(4)");
+const button5 = document.querySelector("button:nth-child(5)");
+const button6 = document.querySelector("button:nth-child(6)");
+const button7 = document.querySelector("button:nth-child(7)");
+const button8 = document.querySelector("button:nth-child(8)");
+const button9 = document.querySelector("button:nth-child(9)");
+const button10 = document.querySelector("button:nth-child(10)");
+const button11 = document.querySelector("button:nth-child(11)");
+const button12 = document.querySelector("button:nth-child(12)");
+const button13 = document.querySelector("button:nth-child(13)");
+
+button1.addEventListener("click", function() {
+    document.body.style.backgroundColor = "pink";
+})
